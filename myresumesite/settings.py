7c6f4e6f813a4be33495525cffbe3d5d546ce7b5
Abdur Rahim <abdurrahimcs50@gmail.com>
2021-01-17 01:53:49 +0600

    resumeheroku

--- myresumesite/settings.py
+++ myresumesite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
